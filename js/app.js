823f492b635b987afcd991e709069c0e2783c64b
AdamKrukowskiWeb <adam.krukowski.web@gmail.com>
2016-08-24 21:21:26 +0200

    canvas itp

--- js/app.js
+++ js/app.js
@@ -13,5 +13,30 @@ BjsApp.init = function(){
   // utworz scene
   var scene = new BABYLON.Scene(engine);
 
-  // zmien kolor tła
-}
+  // stworzenie kamery
+  var camera = new BABYLON. FreeCamera('FreeCamera', new BABYLON.Vector3(0, 1, -15), scene);
+      camera.attachControl(canvas);
+
+  // stworzenie światła
+  var light = new BABYLON.HemisphericLight("Light1", new BABYLON.Vector3(0, 1, 0), scene);
+
+  var ground = BABYLON.Mesh.CreateGround("ground1", 20, 20, 2, scene);
+
+  // nowa figury
+  var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
+  sphere.position.y = 1;
+
+  var sphere2 = BABYLON.Mesh.CreateSphere("sphere2", 16, 4, scene);
+  sphere2.position = new BABYLON.Vector3(3, 3, 3);
+
+  var box = BABYLON.Mesh.CreateBox('box', 1, scene);
+      box.position = new BABYLON.Vector3(4, 2, -5);
+      box.scaling.y = 3;
+      box.rotation.x = 45;
+  
+  var cylinder = BABYLON.Mesh.CreateCylinder('cylinder', 4 , 1, 1, 16, scene)    
+ 
+  engine.runRenderLoop(function(){
+    scene.render();
+  });
+};
